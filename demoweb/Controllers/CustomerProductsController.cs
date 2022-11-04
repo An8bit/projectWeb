179d2048baa1ec89ca096733c6b47c3b500b3d40
An8bit <ttkit35@gmail.com>
2022-11-04 20:54:18 +0700

    up

---
+++ demoweb/Controllers/CustomerProductsController.cs
@@ -0,0 +1,36 @@
+﻿using demoweb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demoweb.Controllers
+{
+    public class CustomerProductsController : Controller
+    {
+        private DBSportStoreEntities db = new DBSportStoreEntities();
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products;
+            return View(products.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new
+                HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+
+}
---
+++ demoweb/Models/CartItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demoweb.Models
+{
+    public class CartItem
+    {
+        DBSportStoreEntities db = new DBSportStoreEntities();
+        public int ProductID { get; set; }
+        public string NamePro { get; set; }
+        public string ImagePro { get; set; }
+        public decimal Price { get; set; }
+        public int Number { get; set; }
+        //Tính FinalPrice = Price * Number
+        public decimal FinalPrice()
+        {
+            return Number * Price;
+        }
+        public CartItem(int ProductID)
+        {
+            this.ProductID = ProductID;
+            var productDB = db.Products.Single(s => s.ProductID ==
+            this.ProductID);
+            this.NamePro = productDB.NamePro;
+            this.ImagePro = productDB.ImagePro;
+            this.Price = (decimal)productDB.Price;
+            this.Number = 1;
+        }
+    }
+}
